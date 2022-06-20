1c83f33fe2687c6636821cc95acb985af9ce40f7
wsq <123123>
2022-06-20 19:17:21 +0800

    121212

--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-24 17:47:30
- * @LastEditTime: 2022-06-20 19:15:55
+ * @LastEditTime: 2022-06-20 19:17:16
  * @LastEditors: wsq 123123
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \express-jwt-study\router.js
@@ -43,7 +43,6 @@ module.exports = (app) => {
     const payload = req.body;
     console.log(payload, "payload");
     let sha1 = crypto.createMenu("sha1", "woshizz123").update(payload).toString();
-    
     res.send(sha1 || "ok");
   })
   app.post("/register", UserController.register); // 用户注册
