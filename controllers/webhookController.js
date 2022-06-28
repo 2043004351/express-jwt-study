96c329738cb0598dc1d54682d83bcb47e48895fb
wsq <123123>
2022-06-28 10:35:46 +0800

    change

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -2,7 +2,7 @@
  * @Author: wsq 123123
  * @Date: 2022-06-21 10:24:58
  * @LastEditors: wsq 123123
- * @LastEditTime: 2022-06-28 10:24:41
+ * @LastEditTime: 2022-06-28 10:34:17
  * @FilePath: \express-jwt-study\controllers\webhookController.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -12,15 +12,15 @@ const { resolve } = require("path");
 module.exports = {
   // 验证token
   verifyToken: async (req, res) => {
-    const sha256 = req.headers["x-hub-signature"].spilt("=")[1];
+    const sha256 = req.headers["x-hub-signature"].split("=")[1];
     if (this.isTokenExist(sha256, JSON.stringify(req.body))) {
       const { ref } = req.body;
-      // if (ref.indexOf("develop") > -1) {
-      //   // 首先设置deploy.sh权限
-      //   shell.exec(`chmod +x ${resolve(__dirname, "../deploy.sh")}`);
-      //   shell.exec(`sh ${resolve(__dirname, "../scripts/deploy.sh")}`);
-      //   res.send("success");
-      // }
+      if (ref.indexOf("develop") > -1) {
+        // 首先设置deploy.sh权限
+        shell.exec(`chmod +x ${resolve(__dirname, "../deploy.sh")}`);
+        shell.exec(`sh ${resolve(__dirname, "../scripts/deploy.sh")}`);
+        res.send("success");
+      }
       res.send("success")
     } else {
       res.status(401).send({
