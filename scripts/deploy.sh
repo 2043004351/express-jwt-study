6e26ff1cf4472f5668d2bb0fcec8a800a483dd24
wsq <123123>
2022-06-28 11:03:57 +0800

    测试

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,12 +2,18 @@
  # @Author: wsq 123123
  # @Date: 2022-06-21 10:44:31
  # @LastEditors: wsq 123123
- # @LastEditTime: 2022-06-28 10:59:43
+ # @LastEditTime: 2022-06-28 11:03:52
  # @FilePath: \express-jwt-study\scripts\deploy.sh
  # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 ### 
+# 进入根目录
+cd /www/wwwroot/express-jwt-study
+# 测试
+echo "进入首页"
 # 首先拉取代码
 git pull
+# 测试
+echo "git pull"
 # 更新npm包
 npm install
 # pm2重启服务
