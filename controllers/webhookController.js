7085e90ff5957b91db44298a158bf6cbd3ff06ca
wsq <123123>
2022-06-28 10:43:46 +0800

    change

--- controllers/webhookController.js
+++ controllers/webhookController.js
@@ -2,7 +2,7 @@
  * @Author: wsq 123123
  * @Date: 2022-06-21 10:24:58
  * @LastEditors: wsq 123123
- * @LastEditTime: 2022-06-28 10:36:52
+ * @LastEditTime: 2022-06-28 10:43:03
  * @FilePath: \express-jwt-study\controllers\webhookController.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -36,12 +36,16 @@ module.exports = {
         shell.exec(`chmod +x ${resolve(__dirname, "../deploy.sh")}`);
         shell.exec(`sh ${resolve(__dirname, "../scripts/deploy.sh")}`);
         res.send("success");
+        return;
+      } else {
+        res.status(401).send({
+          message: "请先登录",
+        });
       }
-      res.send("success");
     } else {
       res.status(401).send({
         message: "请先登录",
       });
     }
-  }
+  },
 };
